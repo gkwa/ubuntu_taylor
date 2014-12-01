71f4ed849c94f2f712d6e3e967fa94ff6c68b641
taylormonacelli <taylormonacelli@gmail.com>
2014-11-30 18:35:30 -0800

    Run apt-get update in quiet mode

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # -*- mode: sh -*-
 
-sudo apt-get update
+sudo apt-get update -qq
 sudo apt-get install -y emacs git lsof
 
 cd ~
 
-git init && git remote add origin https://github.com/taylormonacelli/dotfiles.git
-git fetch && git checkout -f -t origin/master
-emacs --daemon
+git init
+git remote add origin https://github.com/taylormonacelli/dotfiles.git
+git fetch
+git checkout -f -t origin/master
+
+emacs --daemon # this installs melpa packages
